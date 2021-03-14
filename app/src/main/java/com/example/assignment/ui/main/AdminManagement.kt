827e8e927a9c04758484ba3aae9a951edb8f910f
Yih Kit <cheowyk-wm18@student.tarc.edu.my>
2021-03-15 01:38:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/assignment/ui/main/AdminManagement.kt
@@ -0,0 +1,30 @@
+package com.example.assignment.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.assignment.R
+
+class AdminManagement : Fragment() {
+
+    companion object {
+        fun newInstance() = AdminManagement()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.admin_management, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
